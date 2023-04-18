b0811398fe190c70663bb1a9a6ac53272b7e0b14
ClaireSubiali <claire-subiali@hotmail.fr>
2023-04-18 17:06:11 +0200

    ajustements mentions légales et contact/newsletter

--- app/views/legal-mentions.tpl.php
+++ app/views/legal-mentions.tpl.php
@@ -1,1 +1,19 @@
 <h2>Mentions légales</h2>
+<div>« Mentions légales
+
+Nom de l’association :
+
+Adresse :
+
+Numéro de téléphone :
+
+Directeur de la publication (nom du président de l’association) :
+
+Hébergeur :
+
+(Raison sociale)
+
+(Adresse du siège)
+
+(Numéro de téléphone) »
+</div>
--- app/views/newsletter.tpl.php
+++ app/views/newsletter.tpl.php
@@ -1,1 +1,2 @@
 <h2>Inscription à la newsletter</h2>
+<p>[En cours de mise en place]</p>
